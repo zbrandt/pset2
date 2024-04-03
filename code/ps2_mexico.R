8592baa264a84b905364e009a9cfcfca0b4f4108
Zachary Brandt <40130046+zbrandt@users.noreply.github.com>
2024-04-03 13:41:50 -0700

    Question 2 Part F and spelling errors

--- code/ps2_mexico.R
+++ code/ps2_mexico.R
@@ -23,7 +23,6 @@ second_stage <- ivreg(log(inc_m) ~ educ_years | log(dist_us_km), data = mexico)
 stargazer(ols, first_stage, second_stage)
 
 # part g
-
 first_stage_controls <- lm(educ_years ~ log(dist_us_km) + log(sales_hotel) + logtemp + logprecip + ind_lang, mexico)
 reduced_form <- lm(log(inc_m) ~ log(dist_us_km), mexico)
 reduced_form_controls <- lm(log(inc_m) ~ log(dist_us_km) + log(sales_hotel) + logtemp + logprecip + ind_lang, mexico)
